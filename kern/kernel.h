013e7051e66009e289360bfa2a3c76f84e6e9b3f
0x90nz <pbouda14@gmail.com>
2020-07-12 19:00:50 +1200

    Add memory info parsing

--- kern/kernel.h
+++ kern/kernel.h
@@ -8,3 +8,10 @@ uint8_t inb(uint16_t portnumber);
 void hang();
 void hlt();
 void print_hex(int num);
+
+typedef struct {
+    uint16_t extended1;
+    uint16_t extended2;
+    uint16_t configured1;
+    uint16_t configured2;
+} memory_info_t;
