91b9694eff8dd227a69100ba7965a9c467cf8945
0x90nz <pbouda14@gmail.com>
2020-07-14 21:40:02 +1200

    Failed attempt to get packet transmission to work
    I need to read the datasheet closer and actually figure this out...

--- kern/ne2k.c
+++ kern/ne2k.c
@@ -5,6 +5,33 @@
 
 uint16_t iobase;
 
+void tx_packet(void* packet, size_t len)
+{
+    outb(iobase + NE2K_REG_CMD, 0x22); // Start, nodma
+
+    // Load the packet size
+    outb(iobase + NE2K_RBCR0, len & 0xff);
+    outb(iobase + NE2K_RBCR1, (len >> 8) & 0xff);
+
+    outb(iobase + NE2K_REG_INT_STAT, inb(iobase + NE2K_REG_INT_STAT) | (1 << 6));
+
+    outb(iobase + NE2K_RSAR0, 0x00); // Always 0
+    outb(iobase + NE2K_RSAR1, 0x01); // Target page
+
+
+    outb(iobase + NE2K_REG_CMD, 0x12); // start, remote write DMA
+
+    uint8_t* temp_packet = (uint8_t*)packet;
+    for (size_t i = 0; i < len; i++)
+    {
+        outw(iobase + NE2K_REG_DATA, temp_packet[i]);
+    }
+
+    // Wait until bit 6 is set
+    while (inb(iobase + NE2K_REG_INT_STAT) & (1 << 6) == 0) { puts("poll\n");};
+    puts("done tx\n");
+}
+
 void ne2k_init(pci_device_desc_t* device)
 {
     // Get rid of the two least significant bits of the iobase
@@ -54,5 +81,5 @@ void ne2k_init(pci_device_desc_t* device)
     print_hex(prom[5]);
     puts("\n");
 
-
+    tx_packet(prom, 32);
 }
