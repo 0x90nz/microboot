c3871149c55d821d97cb609464b4ac317a4b2fe8
0x90nz <pbouda14@gmail.com>
2020-07-17 08:41:22 +1200

    Fix endianness issue in IP header

--- kern/ether.h
+++ kern/ether.h
@@ -6,7 +6,7 @@
 typedef struct {
     uint8_t dst_mac[6];
     uint8_t src_mac[6];
-    uint16_t length;
+    uint16_t ethertype;
 } __attribute__((packed)) ether_header_t;
 
 void* ether_make_packet(void* buffer, uint8_t* src, uint8_t* dst, uint16_t length);
