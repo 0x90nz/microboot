e1d5fb723e87d9201504613351888a68d9bc65fb
0x90nz <pbouda14@gmail.com>
2020-08-03 20:20:26 +1200

    Change GDT to use a macro for clarity

--- kern/sys/gdt.h
+++ kern/sys/gdt.h
@@ -6,11 +6,17 @@ struct gdt_entry {
     uint32_t base;
     uint32_t limit;
     uint32_t type;
+    uint32_t flags;
 };
 
 struct gdt_ptr {
     uint16_t limit;
     uint32_t base;
 } __attribute__((packed));
+
+enum gdt_flags {
+    GDT_GRANULARITY     = (1 << 7),
+    GDT_SIZE            = (1 << 6)
+};
 
 void gdt_init();
