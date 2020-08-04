236dc3bae54f3fb33d0d537b05f84a0ba5d46af5
Paul Bouda <paul@nop.nz>
2020-08-04 15:23:59 +1200

    Change read to use CHS properly

--- kern/io/bios_drive.h
+++ kern/io/bios_drive.h
@@ -4,4 +4,8 @@
 #include <stddef.h>
 
 void bdrive_init();
-void bdrive_read_sectors(uint8_t drive_number, uint32_t offset, uint8_t* buffer, uint8_t num_sectors);
+void bdrive_read_sectors(
+    uint8_t drive_number, 
+    uint32_t cyl, uint32_t heads, uint32_t sect, 
+    uint8_t* buffer, uint8_t num_sectors
+);
