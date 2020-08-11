d28f90bef214e5b03cef65398ea889a0d3968227
0x90nz <pbouda14@gmail.com>
2020-08-11 18:25:44 +1200

    Add metadata reading from inodes

--- kern/fs/ext2.c
+++ kern/fs/ext2.c
@@ -13,12 +13,30 @@ void read_block(struct ext2_fs* fs, uint32_t block, void* buffer)
     );
 }
 
-void inode_data(struct ext2_fs* fs, uint32_t inode_num)
+void read_inode(struct ext2_fs* fs, struct ext2_inode* inode, uint32_t inode_num)
 {
+    // The group to which this inode belongs
     uint32_t group = inode_num / fs->inodes_per_group;
+    // The block which points to the inode table
     uint32_t inode_tbl_block = fs->bgd_table[group].addr_block_inode_table;
     uint32_t group_idx = inode_num - (group * fs->inodes_per_group);
     
+    // TODO: either account for extended properties not being present, or just
+    // throw a tantrum if they're not present
+    uint32_t block_off = (group_idx - 1) * fs->sb->ext.inode_size / fs->block_size;
+
+    // The offset _within_ the block that we're going to read
+    uint32_t internal_offset = (group_idx - 1) 
+            - block_off * (fs->block_size / fs->sb->ext.inode_size);
+
+    debugf("group: %d, tbl_block: %d, group_idx: %d, block_off: %d, internal_off: %d",
+        group, inode_tbl_block, group_idx, block_off, internal_offset
+    );
+
+    char* buffer = kalloc(fs->block_size);
+    read_block(fs, inode_tbl_block + block_off, buffer);
+    memcpy(inode, buffer + internal_offset * fs->sb->ext.inode_size, fs->sb->ext.inode_size);
+    kfree(buffer);
 }
 
 void ext2_init(uint8_t drive_num, uint32_t start_lba, uint32_t num_sectors)
@@ -45,15 +63,21 @@ void ext2_init(uint8_t drive_num, uint32_t start_lba, uint32_t num_sectors)
     if (fs->blocks_per_group * fs->num_groups < fs->num_groups)
         fs->num_groups++;
 
-
+    // Find the number of fs blocks that we need to read to get the BGD table
     uint32_t bgd_table_blocks = (fs->num_groups * sizeof(struct ext2_bgd)) / fs->block_size;
     if (bgd_table_blocks * fs->block_size < fs->num_groups * sizeof(struct ext2_bgd))
         bgd_table_blocks++;
 
     debugf("num_groups: %d, bgd_table_blocks: %d", fs->num_groups, bgd_table_blocks);
     
+    // Allocate space for the BGD table, and then fill it with data from disk
     fs->bgd_table = kcalloc(bgd_table_blocks * fs->block_size * sizeof(struct ext2_bgd));
     for (uint32_t i = 0; i < bgd_table_blocks; i++) {
         read_block(fs, 2, (void*)fs->bgd_table + i * fs->block_size);
     }
+
+    struct ext2_inode* root_inode = kcalloc(sizeof(struct ext2_inode));
+    read_inode(fs, root_inode, 2);
+
+    debugf("last access time: %d", root_inode->last_access_time);
 }
