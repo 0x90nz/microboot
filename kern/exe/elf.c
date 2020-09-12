e00f304df7938e8f4a50460a7246e80ceb0b4847
Paul Bouda <paul@nop.nz>
2020-09-12 19:42:27 +1200

    Make loaded elf files position independent

--- kern/exe/elf.c
+++ kern/exe/elf.c
@@ -1,20 +1,41 @@
 #include "elf.h"
 #include "../stdlib.h"
+#include "../alloc.h"
 
-void elf_run(void* elf)
+size_t get_elf_size(struct elf_header* hdr)
 {
-    struct elf_header* hdr = (struct elf_header*)elf;
-    struct elf_program_header* phdr = (struct elf_program_header*)(elf + hdr->phoff);
+    struct elf_program_header* phdr = (struct elf_program_header*)((void*)hdr + hdr->phoff);
+    size_t total_size = 0;
     for (int i = 0; i < hdr->phnum; i++, phdr++) {
         switch (phdr->type) {
         case ELF_PT_LOAD:
-            memcpy((void*)phdr->paddr, elf + phdr->offset, phdr->filesz);
+            total_size += phdr->memsz;
             break;
         default:
             break;
         }
     }
 
-    void (*entry)(void) = (void (*)(void))hdr->entry;
+    return total_size;
+}
+
+void elf_run(void* elf)
+{
+    struct elf_header* hdr = (struct elf_header*)elf;
+    struct elf_program_header* phdr = (struct elf_program_header*)(elf + hdr->phoff);
+    void* base = kalloc(get_elf_size(hdr));
+    for (int i = 0; i < hdr->phnum; i++, phdr++) {
+        switch (phdr->type) {
+        case ELF_PT_LOAD:
+            memcpy(base + phdr->paddr, elf + phdr->offset, phdr->filesz);
+            break;
+        default:
+            break;
+        }
+    }
+
+    void (*entry)(void) = (void (*)(void))(base + hdr->entry);
     entry();
+
+    kfree(base);
 }
