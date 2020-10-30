40a36d7f0cd7ac8a1a77bad2061c84eea51dfa6c
Paul Bouda <paul@nop.nz>
2020-10-31 09:25:39 +1300

    Update create image script to not require sudo

--- mkimg.sh
+++ mkimg.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 IMAGE="$1"
+FS_SIZE="30M"
 
-if $(mkdir mnt) ; then
-    dd if=/dev/zero of="$IMAGE" bs=1M count=32
-    echo "o
+rm -f "$IMAGE"
+dd if=/dev/zero of="$IMAGE" bs=1M count=32
+echo "o
     n
     p
     1
     2048
-
+    +$FS_SIZE
     a
     w" | fdisk "$IMAGE" >/dev/null
 
-    LOOP=$(sudo losetup -fP --show "$IMAGE")
-    sudo mkfs.ext2 "${LOOP}p1"
-    sudo mount "${LOOP}p1" mnt
-    sudo cp -r rootfs/* mnt
-    ls -li mnt
-    sudo umount mnt
-    sudo losetup -d "$LOOP"
-    rmdir mnt
-else
-    echo "Cannot finish, mnt already present. Please clean up"
-    exit -1
-fi
+rm -f "build/rootfs.img"
+mke2fs -L 'microboot' -d rootfs -t ext2 "build/rootfs.img" "$FS_SIZE"
+dd if="build/rootfs.img" of="$IMAGE" bs=512 seek=2048 conv=notrunc
