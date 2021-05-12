b2ed6505241e4a34c89806c272a1cd4466b3f499
Paul Bouda <paul@nop.nz>
2021-05-12 17:05:41 +1200

    Implement the rest of linked lists
    Resolves #13

--- kern/list.c
+++ kern/list.c
@@ -12,6 +12,12 @@ void list_init(struct list* list)
     list->tail.next = NULL;
 }
 
+/**
+ * @brief Append an item to a list.
+ *
+ * @param list the list to append to
+ * @param item the item to append to the list
+ */
 void list_append(struct list* list, struct list_node* item)
 {
     if (list->tail.prev) {
@@ -31,6 +37,12 @@ void list_append(struct list* list, struct list_node* item)
     }
 }
 
+/**
+ * @brief Create a list node from a pointer to some data.
+ *
+ * @param data the data to store in the list node
+ * @return struct list_node* the list node representing the data
+ */
 struct list_node* list_node(void* data)
 {
     struct list_node* node = kalloc(sizeof(*node));
@@ -38,39 +50,91 @@ struct list_node* list_node(void* data)
     return node;
 }
 
-void list_remove(struct list* list, struct list_node* item)
+/**
+ * @brief Remove an item from a list.
+ *
+ * @param item the item to remove
+ */
+void list_remove(struct list_node* item)
 {
+    struct list_node* prev = item->prev;
+    struct list_node* next = item->next;
 
+    prev->next = next;
+    next->prev = prev;
+
+    kfree(item);
 }
 
-void list_insert(struct list* list, struct list_node* item)
+/**
+ * @brief Insert an item into a list.
+ *
+ * @param before the item to insert this item before
+ * @param item the item to insert into the list
+ */
+void list_insert(struct list_node* before, struct list_node* item)
 {
-
+    struct list_node* oldnext = before->next;
+    before->next = item;
+    oldnext->prev = item;
 }
 
+/**
+ * @brief Get the next item in a list after the given item.
+ *
+ * @param item the item to get the next item for
+ * @return struct list_node* the next item
+ */
 struct list_node* list_next(struct list_node* item)
 {
     return item->next;
 }
 
+/**
+ * @brief Get the previous item in a list before the given item.
+ *
+ * @param item the item to get the previous item for
+ * @return struct list_node* the previous item
+ */
 struct list_node* list_prev(struct list_node* item)
 {
     return item->prev;
 }
 
+/**
+ * @brief Iterate over a list, calling the consumer for each item
+ *
+ * @param list the list to iterate over
+ * @param consumer the consumer to be called for each item in the list
+ */
 void list_iterate(struct list* list, list_consumer consumer)
 {
-    struct list_node* current = &list->head;
-    while ((current = list_next(current)) && current->next) {
+    LIST_FOREACH(current, list) {
         consumer(current);
     }
 }
 
+/**
+ * @brief Enumerate over a list, calling the enumerator for each item
+ *
+ * @param list the list to enumerate over
+ * @param enumerator the enumerator to call for each item
+ */
 void list_enumerate(struct list* list, list_enumerator enumerator)
 {
-    struct list_node* current = &list->head;
     int i = 0;
-    while ((current = list_next(current))->next) {
+    LIST_FOREACH(current, list) {
         enumerator(i++, current);
     }
+}
+
+/**
+ * @brief Get the head of a list.
+ *
+ * @param list the list to get the head of
+ * @return struct list_node* the head of the list
+ */
+struct list_node* list_head(struct list* list)
+{
+    return &list->head;
 }
--- kern/list.h
+++ kern/list.h
@@ -1,14 +1,12 @@
 #pragma once
 
-struct list_node 
-{
+struct list_node {
     struct list_node* next;
     struct list_node* prev;
     void* value;
 };
 
-struct list
-{
+struct list {
     struct list_node head;
     struct list_node tail;
 };
@@ -18,10 +16,20 @@ typedef void (*list_enumerator)(int, struct list_node*);
 
 void list_init(struct list* list);
 void list_append(struct list* list, struct list_node* item);
-void list_remove(struct list* list, struct list_node* item);
-void list_insert(struct list* list, struct list_node* item);
+void list_remove(struct list_node* item);
+void list_insert(struct list_node* before, struct list_node* item);
 void list_iterate(struct list* list, list_consumer consumer);
 void list_enumerate(struct list* list, list_enumerator enumerator);
 struct list_node* list_prev(struct list_node* item);
 struct list_node* list_next(struct list_node* item);
+struct list_node* list_head(struct list* list);
 struct list_node* list_node(void* data);
+
+/**
+ * @brief Iterate over a list.
+ *
+ * i is the current list item and l is the list.
+ */
+#define LIST_FOREACH(i, l) \
+struct list_node* i = list_head(l); \
+while (i && (i = list_next(i)) && i->next)
