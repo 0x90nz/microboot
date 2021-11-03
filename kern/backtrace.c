33ab81b37b2bcf04487487d216e7669d55e21d57
Paul Bouda <paul@nop.nz>
2021-11-04 11:09:40 +1300

    Reformat backtrace to be more usable with addr2line

--- kern/backtrace.c
+++ kern/backtrace.c
@@ -1,17 +1,22 @@
 #include <stdint.h>
 #include "backtrace.h"
 #include "printf.h"
+#include "stdlib.h"
+#include "io/ansi_colours.h"
 
 void backtrace()
 {
 	printf("\nbacktrace:\n");
+    dprintf_raw(ANSI_COLOR_RED "\nbacktrace: ");
 
 	void** frame;
 	for (frame = __builtin_frame_address(1);
 			(uint32_t)frame >= 0x1000 && frame[0] != NULL;
 			frame = frame[0]) {
 		printf(" %p\n", frame[1]);
-		debugf("%p", frame[1]);
+        dprintf_raw("0x%p ");
 	}
+
+    dprintf_raw(ANSI_COLOR_RESET);
 }
 
