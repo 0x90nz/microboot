bd8d23e9441e33e914fb6b4dc4f5cefb4cf98821
Paul Bouda <paul@nop.nz>
2022-02-20 13:45:50 +1300

    fbcon: Add support for loading and multi-byte fonts
    Previously multi-byte wide fonts were unsupported, and the framebuffer
    console font was always hardcoded. This adds support for character
    bitmaps which may be more than eight pixels wide, as well as dynamically
    loading fonts.

--- kern/io/fbcon.c
+++ kern/io/fbcon.c
@@ -11,26 +11,41 @@ typedef struct {
     uint32_t fg_colour;
     uint32_t bg_colour;
     uint32_t colour_map[16];
+    struct fbcon_font font;
 } fbcon_priv_t;
 
-#define CHAR_HEIGHT 13
-#define CHAR_WIDTH 7
-
+// fb->put_pixel(fb, px_x + x, px_y + y, priv->bg_colour);
 static void fb_putxy(console_t* con, int x_base, int y_base, char c)
 {
     // A mask is theroetically faster than bitshift operations.
     // TODO: check that is actually true
     fbcon_priv_t* priv = con->priv;
     fbdev_t* fb = priv->fb;
-    int mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
-    int px_x = x_base * CHAR_WIDTH;
-    int px_y = y_base * CHAR_HEIGHT;
-    for (int y = 0; y < CHAR_HEIGHT; y++) {
-        for (int x = 0; x < CHAR_WIDTH; x++) {
-            if (font_cherry[(unsigned char)c - 32][y] & mask[x]) {
-                fb->put_pixel(fb,  px_x + x, px_y + y, priv->fg_colour);
+
+    const int mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    const int px_x = x_base * priv->font.char_width;
+    const int px_y = y_base * priv->font.char_height;
+
+    // uint8_t* bitmap = priv->font.data + ((c - 32) * priv->font.char_height);
+    const int char_height = priv->font.char_height;
+    const int char_width = priv->font.char_width;
+    const int char_wbytes = priv->font.char_width_bytes;
+    const uint32_t fg = priv->fg_colour;
+    const uint32_t bg = priv->bg_colour;
+
+    const uint8_t* bitmap = priv->font.data + (char_height * char_wbytes * (c - 32));
+
+    for (int y = 0; y < char_height; y++) {
+        for (int x = 0; x < char_width; x++) {
+            // figure out the index into the bitmap, and the specifc bit within
+            // that byte we're looking at
+            int idx = (y * char_wbytes) + (x >> 3); // x >> 3 == x / 8
+            int off = x & 7; // x % 8
+
+            if (bitmap[idx] & mask[off]) {
+                fb->put_pixel(fb, px_x + x, px_y + y, fg);
             } else {
-                fb->put_pixel(fb, px_x + x, px_y + y, priv->bg_colour);
+                fb->put_pixel(fb, px_x + x, px_y + y, bg);
             }
         }
     }
@@ -70,7 +85,7 @@ static void fb_scroll(console_t* con)
     fbcon_priv_t* priv = con->priv;
     fbdev_t* fb = priv->fb;
     // TODO: replace with bulk copy?
-    fb->shift(fb, CHAR_HEIGHT);
+    fb->shift(fb, priv->font.char_height);
     console_clear_row(con, con->y_pos);
 }
 
@@ -80,8 +95,8 @@ static void fb_invalidate(console_t* con, int x, int y, int width, int height)
     fbdev_t* fb = priv->fb;
     fb->invalidate(
         fb,
-        x * CHAR_WIDTH, y * CHAR_HEIGHT,
-        width * CHAR_WIDTH, height * CHAR_HEIGHT
+        x * priv->font.char_width, y * priv->font.char_height,
+        width * priv->font.char_width, height * priv->font.char_height
     );
 }
 
@@ -121,6 +136,9 @@ static int fbcon_setparam(struct device* dev, int param_id, void* aux)
     case FBCON_SETPARAM_COLOURSCHEME:
         memcpy(priv->colour_map, aux, 16 * sizeof(uint32_t));
         break;
+    case FBCON_SETPARAM_FONT:
+        priv->font = *(struct fbcon_font*)aux;
+        break;
     }
     return 0;
 }
@@ -131,13 +149,19 @@ static struct device* fbcon_create(fbdev_t* fb)
     fbcon_priv_t* priv = kalloc(sizeof(*priv));
 
     priv->fb = fb;
+
+    priv->font.data = (uint8_t*)font_cherry;
+    priv->font.char_width = 7;
+    priv->font.char_width_bytes = 1;
+    priv->font.char_height = 13;
+
     memcpy(priv->colour_map, base_colour_map, sizeof(base_colour_map));
     priv->fg_colour = priv->colour_map[COLOUR_DEFAULT_FG];
     priv->bg_colour = priv->colour_map[COLOUR_DEFAULT_BG];
     con->priv = priv;
 
-    con->width = fb->width / CHAR_WIDTH;
-    con->height = fb->height / CHAR_HEIGHT;
+    con->width = fb->width / priv->font.char_width;
+    con->height = fb->height / priv->font.char_height;
 
     con->put_xy = fb_putxy;
     con->set_cursor = fb_null;
--- kern/io/fbcon.h
+++ kern/io/fbcon.h
@@ -5,5 +5,31 @@
 enum fbcon_setparam_id {
     // aux = array of 16 colours per console.h (enum console_colour)
     FBCON_SETPARAM_COLOURSCHEME = 1,
+    FBCON_SETPARAM_FONT,
+};
+
+struct fbcon_font {
+    // The data associated with the font, with ' ' (32) being the first character
+    // and subsequently following ASCII ordering.
+    //
+    // Each character is made up of `char_height` rows of pixels, where each row
+    // is `char_width` long. The rows are packed into a single byte, where the
+    // bits are displayed MSB to LSB. Not all bits must be used, any unused bits
+    // are ignored, but it would be wise for them to be zero, so that if they happen
+    // to be displayed, it would be blank.
+    //
+    // The font data within a single row is displayed MSB to LSB for the first
+    // byte, then MSB to LSB for the second byte, and so on, until enough bits to
+    // reach char_width_bytes have been read.
+    //
+    // Thus, a single character will take char_width_bytes * char_height bytes.
+    uint8_t* data;
+    // The width of the font in pixels.
+    uint8_t char_width;
+    // The number of bytes comprising a single row of data. May be more than required
+    // for char_width pixels, for example to aid in alignment.
+    uint8_t char_width_bytes;
+    // The width of the font in pixels.
+    uint8_t char_height;
 };
 
